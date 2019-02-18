068ed3e850a725b779bd13bcc4b4e25545650725
mpechaud2017 <maelien.pechaud2017@campus-eni.fr>
2019-02-18 16:13:35 +0100

    Create bo

---
+++ src/fr/eni/sortir/bo/Ville.java
@@ -0,0 +1,40 @@
+package fr.eni.sortir.bo;
+
+import java.io.Serializable;
+
+public class Ville implements Serializable {
+
+    private int idVille;
+    private String nom;
+    private int codePostal;
+
+    public Ville(int idVille, String nom, int codePostal) {
+        this.idVille = idVille;
+        this.nom = nom;
+        this.codePostal = codePostal;
+    }
+
+    public int getIdVille() {
+        return idVille;
+    }
+
+    public void setIdVille(int idVille) {
+        this.idVille = idVille;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+}
