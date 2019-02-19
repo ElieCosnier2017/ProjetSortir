06cccdcbb1e52f9124856261aacd57b872cd071f
ecosnier2017 <ecosnier2017@44-UC45-23.ad.campus-eni.fr>
2019-02-19 10:26:52 +0100

    DAO

--- src/fr/eni/sortir/dal/ListeSortieDAOJdbcImpl.java
+++ src/fr/eni/sortir/dal/ListeSortieDAOJdbcImpl.java
@@ -1,5 +1,35 @@
 package fr.eni.sortir.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.sortir.bo.Sortie;
+
 public class ListeSortieDAOJdbcImpl extends listeSortieDAO {
-	
+	private static final String SELECT_ALL = "SELECT * FROM SORTIES";
+
+	@Override
+	public List<Sortie> selectAll() {
+		List<Sortie> listeSortie = new ArrayList<Sortie>();
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			PreparedStatement pstmt = cnx.prepareStatement(SELECT_ALL);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next())
+			{
+				listeSortie.add(new Sortie(rs.getInt("idSortie")));
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			BusinessException businessException = new BusinessException();
+			businessException.ajouterErreur(CodesResultatDAL.LECTURE_LISTES_ECHEC);
+			throw businessException;
+		}
+		return listesCourse;
+	}
 }
