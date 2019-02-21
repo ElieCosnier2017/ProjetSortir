ca5989ff8b767e5094bca756dc30c0806713c984
maelien <maelien22@gmail.com>
2019-02-21 01:03:42 +0100

    fix

---
+++ src/fr/eni/sortir/servlets/sortie/AddSortieServlet.java
@@ -0,0 +1,76 @@
+package fr.eni.sortir.servlets.sortie;
+
+import fr.eni.sortir.bll.BusinessException;
+import fr.eni.sortir.bll.LieuManager;
+import fr.eni.sortir.bll.SortieManager;
+import fr.eni.sortir.bo.Lieu;
+import fr.eni.sortir.bo.Sortie;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Servlet implementation class Sortir
+ */
+@WebServlet("/sortie/add")
+public class AddSortieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	SortieManager sortieManager = new SortieManager();
+	Sortie sortie;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddSortieServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			LieuManager lieuManager = new LieuManager();
+			List<Lieu> listeLieux = lieuManager.selectAll();
+			request.setAttribute("listeLieux", listeLieux);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+				
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/creerSortie.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String nom = request.getParameter("nom");
+			String dateDebut = request.getParameter("datedebut");
+			String duree = request.getParameter("duree");
+			String dateFin = request.getParameter("datefin");
+			String nbInscription = request.getParameter("nbinscription");
+			String infos = request.getParameter("infos");
+			String photo = request.getParameter("photo");
+			String organisateur = request.getParameter("organisateur");
+			String etat = "Ouverte";
+			int idEtat = 1;
+			int idLieu = 1;
+			
+			sortie = sortieManager.ajouter(nom, Date.valueOf(dateDebut), Integer.valueOf(duree), Date.valueOf(dateFin), Integer.valueOf(nbInscription), infos, etat, photo, organisateur, idLieu, idEtat);	
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
