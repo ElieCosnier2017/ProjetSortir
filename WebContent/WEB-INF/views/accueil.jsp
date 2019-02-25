3f808e25af3f1d58abcad1cbca3dbb92f00b7757
PatriceCasquette <elie.cosnier2017@campus-eni.fr>
2019-02-25 11:49:07 +0100

    Incription sortie + modification detail

--- WebContent/WEB-INF/views/accueil.jsp
+++ WebContent/WEB-INF/views/accueil.jsp
@@ -4,6 +4,6 @@
         <title>Sortir.com - Accueil</title>
     </head>
     <body>
-        <a href="/sortie/detail" class="float-right btn btn-outline-primary" style="margin-left: 10px;">Consulter les sorties</a>
-
+        <a href="/sortie/detail?id=2" class="float-right btn btn-outline-primary" style="margin-left: 10px;">Consulter les sorties</a>
+        <a href="/sortie/inscription" class="float-right btn btn-outline-danger" style="margin-left: 10px;">Inscription</a>
 <%@ include file="../layout/footer.jsp"%>
--- src/fr/eni/sortir/bll/InscriptionManager.java
+++ src/fr/eni/sortir/bll/InscriptionManager.java
@@ -1,7 +1,10 @@
 package fr.eni.sortir.bll;
 
+import fr.eni.sortir.bo.Sortie;
 import fr.eni.sortir.dal.DAOFactory;
 import fr.eni.sortir.dal.InscriptionDAO;
+
+import java.sql.Date;
 
 public class InscriptionManager {
 
@@ -12,6 +15,14 @@ public class InscriptionManager {
     }
 
     public void inscriptionSortie(int idSortie, int idParticipant) throws BusinessException {
-        this.inscriptionDAO.insert(idSortie, idParticipant);
+        if(verifSortieForInscription(idSortie)){
+            this.inscriptionDAO.insert(idSortie, idParticipant);
+        }
+    }
+
+    private boolean verifSortieForInscription(int idSortie){
+        SortieManager sortieManager = new SortieManager();
+        Sortie sortie = sortieManager.selectById(idSortie);
+        return "Ouverte".equals(sortie.getEtat()) && sortie.getDateLimiteInscription().before(new java.sql.Date( new java.util.Date().getTime()));
     }
 }
